a8f5822af0dd0c46e54d5a2b3f912ff6aac8ea8d
Clara Layus <layus.clara@gmail.com>
2024-12-11 20:29:05 +0100

    create pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,17 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./docs"
+  },
   plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
     },
   },
 })
